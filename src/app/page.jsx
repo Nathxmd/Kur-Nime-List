112141538c9d295c438dcf47dcc971542891d290
Nathxmd <nathanmahesa36@gmail.com>
2024-09-21 20:56:59 +0700

    add recomendations

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,29 @@
 import Header from "@/components/AnimeList/header";
 import AnimeList from "@/components/AnimeList";
-import { getAnimeResponse } from "./libs/api-libs";
+import {
+  getAnimeResponse,
+  getMangaResponse,
+  getNestedAnimeResponse,
+  reproduce,
+} from "@/libs/api-libs";
 
 const Page = async () => {
   const topAnime = await getAnimeResponse("top/anime", "limit=8");
+  let recommendedAnime = await getNestedAnimeResponse(
+    "recommendations/anime",
+    "entry"
+  );
+  recommendedAnime = reproduce(recommendedAnime, 4);
 
   return (
     <>
       <section>
         <Header title="Top Anime" linkTitle="View All" linkHref="/populer" />
         <AnimeList api={topAnime} />
+      </section>
+      <section>
+        <Header title="Recommended Anime" />
+        <AnimeList api={recommendedAnime} />
       </section>
     </>
   );
