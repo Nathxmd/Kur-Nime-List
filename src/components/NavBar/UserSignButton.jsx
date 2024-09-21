781ce5f22732022ecf0da88977ce34295ff0f796
Nathxmd <nathanmahesa36@gmail.com>
2024-09-21 23:26:37 +0700

    auth login

---
+++ src/components/NavBar/UserSignButton.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { authUserSession } from "@/libs/auth-libs";
+
+const UserSignButton = async () => {
+  const user = await authUserSession();
+  const actionLabel = user ? "Sign Out" : "Sign In";
+  const actionUrl = user ? "/api/auth/signout" : "/api/auth/signin";
+  return (
+    <div className="flex justify-between gap-2">
+      {user ? (
+        <Link href="/users/dashboard" className="py-1">
+          Dashboard
+        </Link>
+      ) : null}
+      <Link
+        href={actionUrl}
+        className="bg-color-dark text-color-accent py-1 px-12 inline-block rounded"
+      >
+        {actionLabel}
+      </Link>
+    </div>
+  );
+};
+
+export default UserSignButton;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import InputSearch from "./inputSearch";
+import UserSignButton from "./userSignButton";
 
 const NavBar = () => {
   return (
     <header className="bg-color-accent">
       <div className="flex md:flex-row flex-col justify-between md:items-center p-4 gap-2">
         <Link href="/" className="font-bold text-black text-2xl">
-          KUYNIMELIST
+          KURNIMELIST
         </Link>
         <InputSearch />
+        <UserSignButton />
       </div>
     </header>
   );
