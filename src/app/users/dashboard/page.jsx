75a3c1d42416f0964c9086cb53b4206d35fab2b0
Nathxmd <nathanmahesa36@gmail.com>
2024-09-23 19:38:18 +0700

    validasi user

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,13 +1,27 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
   return (
-    <div className="text-color-primary">
-      <h3>DASHBOARD</h3>
-      <h5>WELCOME, {user.name}</h5>
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-xl font-bold">WELCOME, {user.name}</h5>
       <Image src={user.image} alt="..." width={250} height={250} />
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href="/users/dashboard/bookmark"
+          className="bg-color-accent text-color-dark font-bold py-3 px-4 text-xl"
+        >
+          My Bookmark
+        </Link>
+        <Link
+          href="/users/dashboard/comment"
+          className="bg-color-accent text-color-dark font-bold py-3 px-4 text-xl"
+        >
+          My Comments
+        </Link>
+      </div>
     </div>
   );
 };
